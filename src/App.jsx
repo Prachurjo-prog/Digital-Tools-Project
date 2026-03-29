917fbf9e2bbc0d8674668fc80a548fff0adf0022
noob-prachurjo <prachurjobarmon2008@gmail.com>
2026-03-30 00:47:32 +0600

    Full project initiated & navbar section added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import NavBar from './Components/NavBar/NavBar'
+
+function App() {
+
+  return (
+    <>
+    <div className='manrope md:max-w-[80%] mx-auto'>
+      <NavBar/>
+    </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,80 @@
+import logo from '../../assets/DigiTools.png'
+
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <div className="navbar ">
+        <div className="navbar-start flex items-center">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a>Products</a>
+              </li>
+              <li>
+                <a>Features</a>
+              </li>
+              <li>
+                <a>Pricing</a>
+              </li>
+              <li>
+                <a>Testimonials</a>
+              </li>
+              <li>
+                <a>FAQ</a>
+              </li>
+            </ul>
+          </div>
+          <img src={logo} className='w-25 md:w-33' />
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-[15px] font-medium">
+            <li>
+              <a>Products</a>
+            </li>
+            <li>
+              <a>Features</a>
+            </li>
+            <li>
+              <a>Pricing</a>
+            </li>
+            <li>
+              <a>Testimonials</a>
+            </li>
+            <li>
+              <a>FAQ</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end flex items-center gap-4">
+            <a className='hidden lg:block'> Login</a>
+           
+          <a className="btn rounded-2xl bg-gradient-to-r from-purple-500 to-purple-700 text-white">Get Started</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
